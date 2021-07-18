221d8575d327d7d197f52eb453cbc5d5f104b795
ts3486 <taos3486@gmail.com>
2021-07-18 16:08:54 +0900

    minor changes

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,22 +1,27 @@
-import { FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+} from "@material-ui/core";
 import { Container } from "react-bootstrap";
-import styles from "../styles/auth.module.css"
+import styles from "../styles/auth.module.css";
 
 export default function Login() {
-    return (
-      <div className="posts-page">
-        <Container className={styles.loginForm}>
-          <FormControl margin="normal">
-            <InputLabel htmlFor="my-input">Email</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text"></FormHelperText>
-          </FormControl>
-          <FormControl margin="normal">
-            <InputLabel htmlFor="my-input">Password</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text"></FormHelperText>
-          </FormControl>
-        </Container>
-      </div>
-    )
-  }
+  return (
+    <div className="posts-page">
+      <Container className={styles.loginForm}>
+        <FormControl margin="normal">
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <Input id="email" aria-describedby="email" />
+          <FormHelperText id="email"></FormHelperText>
+        </FormControl>
+        <FormControl margin="normal">
+          <InputLabel htmlFor="pw">Password</InputLabel>
+          <Input id="pw" aria-describedby="pw" />
+          <FormHelperText id="pw"></FormHelperText>
+        </FormControl>
+      </Container>
+    </div>
+  );
+}
--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,21 +1,29 @@
-import { FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+} from "@material-ui/core";
 import { Container } from "react-bootstrap";
-import "../styles/posts.module.css";
+import styles from "../styles/auth.module.css";
 
 export default function Register() {
-    return (
+  return (
+    <div className="posts-page">
       <div className="posts-page">
-        <Container>
+        <Container className={styles.loginForm}>
           <FormControl margin="normal">
-            <InputLabel htmlFor="my-input">Email</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text"></FormHelperText>
-
-            <InputLabel htmlFor="my-input">Password</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text"></FormHelperText>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input id="email" aria-describedby="email" />
+            <FormHelperText id="email"></FormHelperText>
+          </FormControl>
+          <FormControl margin="normal">
+            <InputLabel htmlFor="pw">Password</InputLabel>
+            <Input id="pw" aria-describedby="pw" />
+            <FormHelperText id="pw"></FormHelperText>
           </FormControl>
         </Container>
       </div>
-    )
-  }
+    </div>
+  );
+}
